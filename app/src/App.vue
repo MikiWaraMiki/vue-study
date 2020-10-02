07f0d9cb467c5d7dc587571eeb1b072277ab0183
mikity_w <m_waragai@enfactory.co.jp>
2020-10-02 11:23:12 +0900

    add text binding

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <p v-if="msg">
+      {{ msg }}
+    </p>
+    <p v-else>
+      文字が入力されていないで
+    </p>
+    <input type="text" v-model="msg" />
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
