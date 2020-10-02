165e0d4935179137c9f4892ae3505da7aac6e4bc
mikity_w <m_waragai@enfactory.co.jp>
2020-10-02 14:53:19 +0900

    add lifecycle sample

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
-    <input type="number" v-model="arrayNum" placeholder="数字を入力してください" />
-    <button @click="createArray()">FizzBuzz計算</button>
-    <template v-if="nums">
-      <p v-for="num in nums" :key="num">
-        {{ num }}は{{ fizzBuzzText(num) }}
-      </p>
-    </template>
-    <template v-else>
-      <p>数字が入力されていません</p>
-    </template>
+    {{ apiRes }}
   </div>
 </template>
 
@@ -17,22 +8,18 @@
 export default {
   data() {
     return {
-      nums: undefined,
-      arrayNum: undefined,
+      apiRes: ''
     }
   },
-  methods: {
-    createArray() {
-      if( !this.arrayNum || this.arrayNum < 1) return
-
-      const parseIntNumber = parseInt(this.arrayNum)
-      this.nums = [...Array(parseIntNumber).keys()].map(i => ++i)
-      console.log(this.nums)
-    },
-    fizzBuzzText(num) {
-      const fizzBuzzStr = (num % 3 ? '' : 'Fizz') + (num % 5 ? '' : 'Buzz')
-      return fizzBuzzStr || 'なんでもないや'
-    }
+  mounted() {
+    console.log(`mounted start:${this.apiRes}`)
+    this.apiRes = 'fugafuga'
+    console.log(`mounted end: ${this.apiRes}`)
+  },
+  created() {
+    console.log(`created start:${this.apiRes}`)
+    this.apiRes = 'hogehoge'
+    console.log(`mounted end: ${this.apiRes}`)
   },
 }
 </script>
